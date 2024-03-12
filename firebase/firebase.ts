47eeda28ffba06c04e6468dce8ef77bc98faef00
Natan <natan.devback@gmail.com>
2024-03-11 21:05:00 -0300

    firebase-Authentication
    Com ExpressJS

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,17 +1,52 @@
-import exp from "constants";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import express from "express";
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	onAuthStateChanged,
+	getIdToken,
+} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { app } from "firebase-admin";
 
-const auth = getAuth();
-createUserWithEmailAndPassword(auth,"1234567890-","234567")
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+const firebaseConfig = {
+	apiKey: "AIzaSyC4UT8VY4gHio4q5N2i5bdNAQLcdJ8V0kQ",
+	authDomain: "test-ace4f.firebaseapp.com",
+	databaseURL: "https://test-ace4f-default-rtdb.firebaseio.com",
+	projectId: "test-ace4f",
+	storageBucket: "test-ace4f.appspot.com",
+	messagingSenderId: "1026816016824",
+	appId: "1:1026816016824:web:6d70b9d81441bafffd8f4d",
+	measurementId: "G-FK0YJLM4YN",
+};
 
-  
+const exp = express();
+exp.use(express.json());
+exp.post("/", (req, res) => {
+	const { email, password } = req.body;
+	const user = {
+		email,
+		password,
+	};
+	const app = initializeApp(firebaseConfig);
+	const auth = getAuth();
+	createUserWithEmailAndPassword(auth, user.email, user.password)
+		.then((userCredential) => {
+			// Signed in
+			const user = userCredential.user;
+			console.log(user);
+      const token = getIdToken(user).then((token) => {
+        console.log(token);
+      });
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// ..
+		});
+    
+});
+
+exp.get("/", (req, res) => {
+   
+});
+exp.listen(3000);
